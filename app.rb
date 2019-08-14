24336c7a00dfd5e1a2521cba53115f1c59f380b9
kierepkae <kierepkaedyta@gmail.com>
2019-08-14 13:06:08 +0100

    add test for deleting bookmark

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'sinatra/base'
 require_relative './lib/bookmarks.rb'
+require 'pg'
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions, :method_override
+
   get '/' do
     'Bookmark Manager'
   end
@@ -16,8 +20,13 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  delete '/bookmarks/:id' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("DELETE FROM bookmarks WHERE id = #{params['id']}")
+    redirect '/bookmarks'
+
   run! if app_file == $PROGRAM_NAME
 end
 
 
-
+end
