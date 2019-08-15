fea93025af4b5c8325d6e5d356edd94d9f33f229
kierepkae <kierepkaedyta@gmail.com>
2019-08-15 17:07:55 +0100

    add happy path for user sign up

--- app.rb
+++ app.rb
@@ -77,6 +77,16 @@ class BookmarkManager < Sinatra::Base
     redirect('/bookmarks')
   end
 
+  get '/sessions/new' do
+    erb :new_session
+  end
+
+   post '/sessions' do
+    user = User.authenticate(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect('/bookmarks')
+  end
+
 
   run! if app_file == $PROGRAM_NAME
 
