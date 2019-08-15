720e7b85dd8c154d49b311715c6320825ca525f9
kierepkae <kierepkaedyta@gmail.com>
2019-08-15 10:41:37 +0100

    remove @connection method from DatabaseConnection class

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -8,12 +8,6 @@ describe DatabaseConnection do
       DatabaseConnection.setup('bookmark_manager_test')
     end
 
-    it 'this connection is persistent' do
-      # Grab the connection as a return value from the .setup method
-      connection = DatabaseConnection.setup('bookmark_manager_test')
-
-      expect(DatabaseConnection.connection).to eq connection
-    end
   end
 
   describe '.query' do
